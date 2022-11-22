ef58ede366e8e758465c97a836df5cec36c5376d
Danilo Silva <danilo.silva@msn.com>
2022-11-22 14:52:32 -0300

    Aula sobre Constantes

---
+++ Fundamentos_CSharp/03_Constantes/Fundamentos/MeuApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MeuApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            const int IDADE_MINIMA = 25; // Correto - Inicia com 25
+            // const int IDADE_MINIMA; // Errado
+            // const var IDADE_MINIMA = 25; // Errado
+            // const var IDADE_MINIMA; // Errado
+
+            var texto = "Testando";
+            Console.WriteLine(texto);
+        }
+    }
+}
