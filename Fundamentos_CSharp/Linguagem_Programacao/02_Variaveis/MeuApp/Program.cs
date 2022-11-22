d39ab5119c21d4bcdb03fc31e1c09670d7b772d8
Danilo Silva <danilo.silva@msn.com>
2022-11-22 13:29:44 -0300

    Aula sobre Variáveis

---
+++ Fundamentos_CSharp/Linguagem_Programacao/02_Variaveis/MeuApp/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace MeuApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int idade;         // Correto - Inicia com 0
+            // int idade = 25; // Correto - Inicia com 25
+            // var idade = 25; // Correto - Inicia com 25
+            // var idade;      // Errado
+
+            var texto = "Testando";
+            System.Console.WriteLine(texto);
+        }
+    }
+}
