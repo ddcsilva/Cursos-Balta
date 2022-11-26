d886ea6783d234af6aa8f988bce2ff66482fec98
Danilo Silva <danilo.silva@msn.com>
2022-11-26 16:38:41 -0300

    Aula sobre Using e Dispose

---
+++ Fundamentos_OO/11_Using_Dispose/Pagamentos/Program.cs
@@ -0,0 +1,20 @@
+﻿System.Console.WriteLine("Hello World!");
+
+using (var pagamento = new Pagamento())
+{
+    System.Console.WriteLine("Processando o pagamento!");
+}
+
+public class Pagamento : IDisposable
+{
+    // Garbage Collection
+    public Pagamento()
+    {
+        System.Console.WriteLine("Iniciando o Pagamento");
+    }
+
+    public void Dispose()
+    {
+        System.Console.WriteLine("Finalizando o Pagamento");
+    }
+}
