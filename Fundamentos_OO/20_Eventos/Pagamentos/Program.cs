5a9b4944b564107c0c4864bedf183bbf4b77eafe
Danilo Silva <danilo.silva@msn.com>
2022-11-26 18:10:51 -0300

    Aula sobre Events

---
+++ Fundamentos_OO/20_Eventos/Pagamentos/Program.cs
@@ -0,0 +1,47 @@
+﻿var sala = new Sala(3);
+sala.SalaCheiaEvent += AoSalaFicarCheia;
+
+sala.ReservarAssento();
+sala.ReservarAssento();
+sala.ReservarAssento();
+sala.ReservarAssento();
+sala.ReservarAssento();
+
+static void AoSalaFicarCheia(object sender, EventArgs e)
+{
+    System.Console.WriteLine("Sala está cheia!");
+}
+
+public class Sala
+{
+    public Sala(int assentos)
+    {
+        this.Assentos = assentos;
+        assentosEmUso = 0;
+    }
+
+    private int assentosEmUso = 0;
+    public int Assentos { get; set; }
+
+    public void ReservarAssento()
+    {
+        assentosEmUso++;
+
+        if (assentosEmUso >= Assentos)
+        {
+            AoSalaFicarCheia(EventArgs.Empty);
+        }
+        else
+        {
+            System.Console.WriteLine("Assento Reservado!");
+        }
+    }
+
+    public event EventHandler SalaCheiaEvent;
+
+    protected virtual void AoSalaFicarCheia(EventArgs e)
+    {
+        EventHandler handler = SalaCheiaEvent;
+        handler?.Invoke(this, e);
+    }
+}
