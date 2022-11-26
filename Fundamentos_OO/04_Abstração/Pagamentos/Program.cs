688acba81db1a6fb7ee4295e18f022a88c96fb3d
Danilo Silva <danilo.silva@msn.com>
2022-11-26 09:23:24 -0300

    Aula sobre Abstração

---
+++ Fundamentos_OO/04_Abstração/Pagamentos/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, World!");
+
+class Pagamento 
+{
+    // Propriedades
+    DateTime Vencimento;
+
+    // Métodos
+    void Pagar() 
+    {
+        ConsultarSaldoDoCartao("123456");
+    }
+
+    private void ConsultarSaldoDoCartao(string numero) 
+    {
+
+    }
+}
