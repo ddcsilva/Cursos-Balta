ac3d9c4140a937a6aa2dabd103339169feaed91d
Danilo Silva <danilo.silva@msn.com>
2022-11-26 17:23:37 -0300

    Aula sobre Interfaces

---
+++ Fundamentos_OO/15_Interfaces/Pagamentos/Program.cs
@@ -0,0 +1,28 @@
+﻿System.Console.WriteLine("Hello World");
+
+public class Pagamento : IPagamento
+{
+    public DateTime Vencimento { get; set; }
+
+    public void Pagar(double valor)
+    {
+
+    }
+}
+
+public class PagamentoCartaoCredito : IPagamento
+{
+    public DateTime Vencimento { get; set; }
+
+    public void Pagar(double valor)
+    {
+
+    }
+}
+
+public interface IPagamento
+{
+    DateTime Vencimento { get; set; }
+
+    void Pagar(double valor);
+}
