afd6113600517de31a9810ad42ae568608eb4525
Danilo Silva <danilo.silva@msn.com>
2022-11-26 14:38:55 -0300

    Aula sobre Polimorfismo

---
+++ Fundamentos_OO/06_Polimorfismo/Pagamentos/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.WriteLine("Hello, World!");
+var pagamentoBoleto = new PagamentoBoleto();
+pagamentoBoleto.Pagar();
+pagamentoBoleto.Vencimento = DateTime.Now;
+pagamentoBoleto.NumeroBoleto = "1234";
+
+var pagamento = new Pagamento();
+pagamento.ToString();
+
+class Pagamento 
+{
+    // Propriedades
+    public DateTime Vencimento;
+
+    // Métodos
+    public virtual void Pagar() {}
+}
+
+class PagamentoBoleto : Pagamento
+{
+    public string NumeroBoleto;
+
+    public override void Pagar()
+    {
+        // Regra do Boleto
+    }
+
+    public override string ToString()
+    {
+        return Vencimento.ToString("dd/MM/yyyy");
+    }
+}
+
+class PagamentoCartaoCredito : Pagamento 
+{
+    public string Numero;
+
+    public override void Pagar()
+    {
+        // Regra do Cartão de Crédito
+    }
+}
