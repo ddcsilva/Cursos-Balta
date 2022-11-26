6ca2c7493235c7f1ebcd0d973d03c30ad4f6cba7
Danilo Silva <danilo.silva@msn.com>
2022-11-26 15:31:51 -0300

    Aula sobre Métodos

---
+++ Fundamentos_OO/10_Métodos/Pagamentos/Program.cs
@@ -0,0 +1,46 @@
+﻿var pagamento = new Pagamento(DateTime.Now);
+pagamento.Pagar("123");
+
+var pagamentoCartao = new PagamentoCartao(DateTime.Now);
+pagamentoCartao.Pagar("123");
+
+public class Pagamento
+{
+    public DateTime DataPagamento { get; set; }
+
+    public Pagamento() { }
+
+    public Pagamento(DateTime dataPagamento)
+    {
+        System.Console.WriteLine("Iniciando o Pagamento");
+        DataPagamento = DateTime.Now;
+    }
+
+    // Sobrecarga de Métodos
+    public virtual void Pagar(string numero)
+    {
+        System.Console.WriteLine("Pagar");
+    }
+    void Pagar(string numero, DateTime dataVencimento) { }
+    void Pagar(string numero, DateTime dataVencimento, bool pagarAposVencimento = false) { }
+}
+
+public class PagamentoCartao : Pagamento
+{
+    public PagamentoCartao(DateTime vencimento) : base(vencimento)
+    {
+        System.Console.WriteLine("Iniciando o Pagamento");
+        DataPagamento = DateTime.Now;
+    }
+
+    // Sobrescrita de Método
+    public override void Pagar(string numero)
+    {
+        System.Console.WriteLine("Pagar Cartão");
+    }
+}
+
+public class Endereco
+{
+    string Cep;
+}
