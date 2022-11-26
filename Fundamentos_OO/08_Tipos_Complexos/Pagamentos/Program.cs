f68dfc41fd1c706f0f032baa6b0879c22ad7681d
Danilo Silva <danilo.silva@msn.com>
2022-11-26 14:56:24 -0300

    Aula sobre Tipos Complexos

---
+++ Fundamentos_OO/08_Tipos_Complexos/Pagamentos/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World!");
+var pagamento = new Pagamento();
+
+public class Pagamento
+{
+    DateTime Vencimento;
+    Endereco enderecoPagamento;
+
+    void Pagar() { }
+}
+
+public class Endereco
+{
+    string Cep;
+}
