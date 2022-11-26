ecf011d9ab6bef83f8b1829412fa7663dc49b2a7
Danilo Silva <danilo.silva@msn.com>
2022-11-26 20:23:41 -0300

    Aula sobre Genericis - Parte 1

---
+++ Fundamentos_OO/21_Genericos_Parte1/Pagamentos/Program.cs
@@ -0,0 +1,24 @@
+﻿var pessoa = new Pessoa();
+var pagamento = new Pagamento();
+var assinatura = new Assinatura();
+
+// var context = new DataContext<Pessoa>();
+var context = new DataContext<Pessoa, Pagamento, Assinatura>();
+context.Salvar(pessoa);
+
+public class DataContext<P, PA, A>
+{
+    public void Salvar(P entity) { }
+
+    public void Alterar(P entity) { }
+
+    public void Alterar(PA entity) { }
+
+    public void Alterar(A entity) { }
+}
+
+public class Pessoa { }
+
+public class Pagamento { }
+
+public class Assinatura { }
