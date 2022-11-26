fb4545f1b97d6ad9266d6b041fbb77ac82622c1c
Danilo Silva <danilo.silva@msn.com>
2022-11-26 17:02:00 -0300

    Aula sobre Classes Estáticas

---
+++ Fundamentos_OO/12_Classes_Estaticas/Pagamentos/Program.cs
@@ -0,0 +1,12 @@
+﻿System.Console.WriteLine("Hello World!");
+Pagamento.Vencimento = DateTime.Now;
+
+public static class Pagamento
+{
+    public static DateTime Vencimento { get; set; }
+}
+
+public static class Configuracoes
+{
+    public static string API_URL { get; set; }
+}
