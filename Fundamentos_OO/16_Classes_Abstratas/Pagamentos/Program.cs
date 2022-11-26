ce8c51e814f9cfdc301da2de0c81992001d89d4e
Danilo Silva <danilo.silva@msn.com>
2022-11-26 17:31:15 -0300

    Aula sobre Classes Abstratas

---
+++ Fundamentos_OO/16_Classes_Abstratas/Pagamentos/Program.cs
@@ -0,0 +1,42 @@
+﻿System.Console.WriteLine("Hello World");
+
+public abstract class Pagamento : IPagamento
+{
+    public DateTime Vencimento { get; set; }
+
+    public virtual void Pagar(double valor)
+    {
+        // Executar algo
+    }
+}
+
+public class PagamentoCartaoCredito : Pagamento
+{
+    public override void Pagar(double valor)
+    {
+        base.Pagar(valor);
+    }
+}
+
+public class PagamentoBoleto : Pagamento
+{
+    public override void Pagar(double valor)
+    {
+        base.Pagar(valor);
+    }
+}
+
+public class PagamentoGooglePay : Pagamento
+{
+    public override void Pagar(double valor)
+    {
+        base.Pagar(valor);
+    }
+}
+
+public interface IPagamento
+{
+    DateTime Vencimento { get; set; }
+
+    void Pagar(double valor);
+}
