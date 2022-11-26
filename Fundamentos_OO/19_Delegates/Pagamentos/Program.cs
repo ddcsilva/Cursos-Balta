fcbd38c2a8f135b87bc242c685e2df937e97bd0c
Danilo Silva <danilo.silva@msn.com>
2022-11-26 17:57:11 -0300

    Aula sobre Delegates

---
+++ Fundamentos_OO/19_Delegates/Pagamentos/Program.cs
@@ -0,0 +1,12 @@
+﻿static void RealizarPagamento(double valor)
+{
+    System.Console.WriteLine($"Pago o valor de {valor}");
+}
+
+var pagar = new Pagamento.Pagar(RealizarPagamento);
+pagar(35);
+
+public class Pagamento
+{
+    public delegate void Pagar(double valor);
+}
