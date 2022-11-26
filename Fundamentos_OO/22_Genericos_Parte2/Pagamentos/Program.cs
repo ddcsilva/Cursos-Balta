33b0794164c7e578a7c6c49bb4bebd9255f24dac
Danilo Silva <danilo.silva@msn.com>
2022-11-26 20:27:44 -0300

    Aula sobre Generics - Parte 2

---
+++ Fundamentos_OO/22_Genericos_Parte2/Pagamentos/Program.cs
@@ -0,0 +1,28 @@
+﻿var pessoa = new Pessoa();
+var pagamento = new Pagamento();
+var assinatura = new Assinatura();
+
+var context = new DataContext<Pessoa, Pagamento, Assinatura>();
+context.Salvar(pessoa);
+context.Salvar(pagamento);
+context.Salvar(assinatura);
+
+public class DataContext<P, PA, A>
+    where P : IPessoa
+    where PA : Pagamento
+    where A : Assinatura
+{
+    public void Salvar(P entity) { }
+
+    public void Salvar(PA entity) { }
+
+    public void Salvar(A entity) { }
+}
+
+public interface IPessoa { }
+
+public class Pessoa : IPessoa { }
+
+public class Pagamento { }
+
+public class Assinatura { }
