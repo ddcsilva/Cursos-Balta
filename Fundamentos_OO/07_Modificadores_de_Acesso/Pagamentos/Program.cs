33662406185299f8b04d6503a349e3882dfc7603
Danilo Silva <danilo.silva@msn.com>
2022-11-26 14:52:27 -0300

    Aula sobre Modificadores de Acesso

---
+++ Fundamentos_OO/07_Modificadores_de_Acesso/Pagamentos/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, World!");
+var pagamento = new Pagamento();
+
+// private, protected, internal e public
+public class Pagamento
+{
+    protected DateTime Vencimento;
+
+    private void Pagar() { }
+}
+
+public class PagamentoBoleto : Pagamento
+{
+    void Teste()
+    {
+        base.Vencimento = DateTime.Now;
+    }
+}
