7c9a353a82aeec73c0bcd5595a054ee9cfab60ec
Danilo Silva <danilo.silva@msn.com>
2022-11-26 17:41:05 -0300

    Aula sobre Upcast e Downcast

---
+++ Fundamentos_OO/17_Upcast_e_Downcast/Pagamentos/Program.cs
@@ -0,0 +1,26 @@
+﻿System.Console.WriteLine("Hello World");
+var pessoa = new Pessoa();
+var pessoaFisica = new PessoaFisica();
+var pessoaJuridica = new PessoaJuridica();
+
+pessoa = new PessoaFisica();
+pessoa = new Pessoa();
+pessoa = new PessoaJuridica();
+pessoa = new Pessoa();
+
+pessoaFisica = (PessoaFisica)pessoa;
+
+public class Pessoa
+{
+    public string Nome { get; set; }
+}
+
+public class PessoaFisica : Pessoa
+{
+    public string CPF { get; set; }
+}
+
+public class PessoaJuridica : Pessoa
+{
+    public string CNPJ { get; set; }
+}
