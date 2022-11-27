51b65488387c3aa33b8f1f9b93f77a7d6e55f074
Danilo Silva <danilo.silva@msn.com>
2022-11-27 16:22:39 -0300

    Adição de Tabelas

--- Fundamentos_SQL_Server/Create_Database.sql
+++ Fundamentos_SQL_Server/Create_Database.sql
@@ -1,9 +1,31 @@
 USE [Curso]
 
+DROP TABLE [Aluno]
+
 CREATE TABLE [Aluno](
-    [Id] INT,
-    [Nome] NVARCHAR,
-    [Nascimento] DATETIME,
-    [Ativo] BIT
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [Email] VARCHAR(180) NOT NULL UNIQUE,
+    [Nascimento] DATETIME NULL,
+    [Ativo] BIT NOT NULL DEFAULT(0),
+    
+    CONSTRAINT [PK_Aluno] PRIMARY KEY([Id]),
+    CONSTRAINT [UQ_Aluno_Email] UNIQUE([Email])
 )
 GO
+
+CREATE TABLE [Curso](
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    
+    CONSTRAINT [PK_Curso] PRIMARY KEY([Id])
+)
+
+GO
+
+CREATE TABLE [ProgressoCurso](
+    [AlunoId] INT NOT NULL,
+    [CursoId] INT NOT NULL,
+    [Progresso] INT NOT NULL,
+    [UltimaAtualizacao] DATETIME NOT NULL DEFAULT(GETDATE())
+)
