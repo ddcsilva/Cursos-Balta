a247328845ed579f566540722b6316fec5a25455
Danilo Silva <danilo.silva@msn.com>
2022-11-27 17:30:50 -0300

    Instalação do Dapper

---
+++ Acesso_Dados_NET_CSharp_Dapper_SQL_Server/02_Dapper/BaltaDataAccess/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+
+namespace BaltaDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=Ra170115!";
+
+            using (var connection = new SqlConnection(connectionString))
+            {
+
+            }
+        }
+    }
+}
