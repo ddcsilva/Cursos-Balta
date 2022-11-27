df8e81d711a17fe6ee33067e02361ac4b9d28e41
Danilo Silva <danilo.silva@msn.com>
2022-11-27 16:46:01 -0300

    Criando a Connection String

---
+++ Acesso_Dados_NET_CSharp_Dapper_SQL_Server/01_Introducao/BaltaDataAccess/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BaltaDataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=Ra170115!";
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
