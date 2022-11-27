8a56115f81f166cecc1b3febe9c20bdc7b651f5f
Danilo Silva <danilo.silva@msn.com>
2022-11-27 17:08:59 -0300

    Utilizando SqlCommand e SqlDataReader

--- Acesso_Dados_NET_CSharp_Dapper_SQL_Server/01_Introducao/BaltaDataAccess/Program.cs
+++ Acesso_Dados_NET_CSharp_Dapper_SQL_Server/01_Introducao/BaltaDataAccess/Program.cs
@@ -9,9 +9,24 @@ namespace BaltaDataAccess
         {
             const string connectionString = "Server=localhost,1433;Database=balta;User ID=sa;Password=Ra170115!";
 
-            using (var connection = new SqlConnection())
+            using (var connection = new SqlConnection(connectionString))
             {
                 Console.WriteLine("Conectado!");
+                connection.Open();
+
+                using (var command = new SqlCommand())
+                {
+                    command.Connection = connection;
+                    command.CommandType = System.Data.CommandType.Text;
+                    command.CommandText = "SELECT [Id], [Title] FROM [Category]";
+
+                    var reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        Console.WriteLine($"{reader.GetGuid(0)} - {reader.GetString(1)}");
+                    }
+                }
             }
         }
     }
