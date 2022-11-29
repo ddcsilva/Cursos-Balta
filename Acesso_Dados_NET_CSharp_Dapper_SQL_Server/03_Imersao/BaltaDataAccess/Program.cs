59375a8bd3f9330d3c45771f9a3df9de07027299
Danilo Silva <danilo.silva@msn.com>
2022-11-28 22:04:42 -0300

    Select IN

--- Acesso_Dados_NET_CSharp_Dapper_SQL_Server/03_Imersao/BaltaDataAccess/Program.cs
+++ Acesso_Dados_NET_CSharp_Dapper_SQL_Server/03_Imersao/BaltaDataAccess/Program.cs
@@ -27,7 +27,8 @@ namespace BaltaDataAccess
                 // ReadView(connection);
                 // OneToOne(connection);
                 // OneToMany(connection);
-                QueryMutiple(connection);
+                // QueryMutiple(connection);
+                SelectIn(connection);
             }
         }
 
@@ -311,6 +312,25 @@ namespace BaltaDataAccess
                     Console.WriteLine(item.Title);
                 }
             }
+        }
+
+        static void SelectIn(SqlConnection connection)
+        {
+            var query = @"select * from Career where [Id] IN @Id";
+
+            var items = connection.Query<Career>(query, new
+            {
+                Id = new[]{
+                    "4327ac7e-963b-4893-9f31-9a3b28a4e72b",
+                    "e6730d1c-6870-4df3-ae68-438624e04c72"
+                }
+            });
+
+            foreach (var item in items)
+            {
+                Console.WriteLine(item.Title);
+            }
+
         }
     }
 }
