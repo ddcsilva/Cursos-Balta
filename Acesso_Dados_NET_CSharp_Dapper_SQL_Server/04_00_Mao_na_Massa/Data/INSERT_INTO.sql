c51df02952ac3f5c370b6f7259a5b36b990801e4
Danilo Silva <danilo.silva@msn.com>
2022-12-01 15:38:42 -0300

    Criando Tabelas e Dapper Contrib

---
+++ Acesso_Dados_NET_CSharp_Dapper_SQL_Server/04_00_Mao_na_Massa/Data/INSERT_INTO.sql
@@ -0,0 +1,12 @@
+USE [Blog]
+GO
+
+INSERT INTO 
+    [Usuario]
+VALUES (
+    'Danilo Silva',
+    'danilo.silva@msn.com',
+    'HASH',
+    'Um mero desenvolvedor',
+    'https://',
+    'danilo-silva')
