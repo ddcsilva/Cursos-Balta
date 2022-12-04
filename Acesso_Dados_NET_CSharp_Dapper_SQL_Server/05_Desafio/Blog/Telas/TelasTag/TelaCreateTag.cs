25aca0f28eae7ef573390980ea42e3f500f8560e
Danilo Silva <danilo.silva@msn.com>
2022-12-04 02:28:00 -0300

    Criando uma tag

--- Acesso_Dados_NET_CSharp_Dapper_SQL_Server/05_Desafio/Blog/Telas/TelasTag/TelaCreateTag.cs
+++ Acesso_Dados_NET_CSharp_Dapper_SQL_Server/05_Desafio/Blog/Telas/TelasTag/TelaCreateTag.cs
@@ -1,4 +1,6 @@
 using System;
+using Blog.Models;
+using Blog.Repositories;
 
 namespace Blog.Telas.TelasTag
 {
@@ -6,7 +8,38 @@ namespace Blog.Telas.TelasTag
     {
         public static void Carregar()
         {
+            Console.Clear();
+            Console.WriteLine("Nova tag");
+            Console.WriteLine("-------------");
+            Console.Write("Nome: ");
+            var nome = Console.ReadLine();
 
+            Console.Write("Slug: ");
+            var slug = Console.ReadLine();
+
+            Create(new Tag
+            {
+                Nome = nome,
+                Slug = slug
+            });
+
+            Console.ReadKey();
+            TelaMenuTag.Carregar();
+        }
+
+        public static void Create(Tag tag)
+        {
+            try
+            {
+                var repository = new Repository<Tag>(Database.Connection);
+                repository.Create(tag);
+                Console.WriteLine("Tag cadastrada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Não foi possível salvar a tag");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
