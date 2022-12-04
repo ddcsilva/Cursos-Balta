63fc139258d00d388154e42f390978f1d149fcc4
Danilo Silva <danilo.silva@msn.com>
2022-12-04 02:31:23 -0300

    Atualizando e Excluindo uma tag

--- Acesso_Dados_NET_CSharp_Dapper_SQL_Server/05_Desafio/Blog/Telas/TelasTag/TelaDeleteTag.cs
+++ Acesso_Dados_NET_CSharp_Dapper_SQL_Server/05_Desafio/Blog/Telas/TelasTag/TelaDeleteTag.cs
@@ -1,4 +1,6 @@
 using System;
+using Blog.Models;
+using Blog.Repositories;
 
 namespace Blog.Telas.TelasTag
 {
@@ -6,6 +8,30 @@ namespace Blog.Telas.TelasTag
     {
         public static void Carregar()
         {
+            Console.Clear();
+            Console.WriteLine("Excluir uma tag");
+            Console.WriteLine("-------------");
+            Console.Write("Qual o id da Tag que deseja exluir? ");
+            var id = Console.ReadLine();
+
+            Delete(int.Parse(id));
+            Console.ReadKey();
+            TelaMenuTag.Carregar();
+        }
+
+        public static void Delete(int id)
+        {
+            try
+            {
+                var repository = new Repository<Tag>(Database.Connection);
+                repository.Delete(id);
+                Console.WriteLine("Tag exluída com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Não foi possível exluir a tag");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
--- Acesso_Dados_NET_CSharp_Dapper_SQL_Server/05_Desafio/Blog/Telas/TelasTag/TelaUpdateTag.cs
+++ Acesso_Dados_NET_CSharp_Dapper_SQL_Server/05_Desafio/Blog/Telas/TelasTag/TelaUpdateTag.cs
@@ -1,4 +1,6 @@
 using System;
+using Blog.Models;
+using Blog.Repositories;
 
 namespace Blog.Telas.TelasTag
 {
@@ -6,6 +8,41 @@ namespace Blog.Telas.TelasTag
     {
         public static void Carregar()
         {
+            Console.Clear();
+            Console.WriteLine("Atualizando uma tag");
+            Console.WriteLine("-------------");
+            Console.Write("Id: ");
+            var id = Console.ReadLine();
+
+            Console.Write("Nome: ");
+            var nome = Console.ReadLine();
+
+            Console.Write("Slug: ");
+            var slug = Console.ReadLine();
+
+            Update(new Tag
+            {
+                Id = int.Parse(id),
+                Nome = nome,
+                Slug = slug
+            });
+            Console.ReadKey();
+            TelaMenuTag.Carregar();
+        }
+
+        public static void Update(Tag tag)
+        {
+            try
+            {
+                var repository = new Repository<Tag>(Database.Connection);
+                repository.Update(tag);
+                Console.WriteLine("Tag atualizada com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Não foi possível atualizar a tag");
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
