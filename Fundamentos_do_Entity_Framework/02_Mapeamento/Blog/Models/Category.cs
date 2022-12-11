4c3625ac7528996236ba31b45282f933c00bfe2c
Danilo Silva <danilo.silva@msn.com>
2022-12-11 11:50:46 -0300

    Required, MaxLenght e Column

--- Fundamentos_do_Entity_Framework/02_Mapeamento/Blog/Models/Category.cs
+++ Fundamentos_do_Entity_Framework/02_Mapeamento/Blog/Models/Category.cs
@@ -10,7 +10,17 @@ namespace Blog.Models
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Name", TypeName = "NVARCHAR")]
         public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(80)]
+        [Column("Name", TypeName = "VARCHAR")]
         public string Slug { get; set; }
     }
 }
