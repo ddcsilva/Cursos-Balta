b87015e09a6cd1f2ee0e12419383733ac1a78b48
Danilo Silva <danilo.silva@msn.com>
2022-12-11 15:19:32 -0300

    Trablhando com Sub Conjuntos

--- Fundamentos_do_Entity_Framework/02_Mapeamento/Blog/Program.cs
+++ Fundamentos_do_Entity_Framework/02_Mapeamento/Blog/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Linq;
 using Blog.Data;
 using Blog.Models;
-using Microsoft.EntityFrameworkCore;
 
 namespace Blog
 {
@@ -10,49 +8,38 @@ namespace Blog
     {
         static void Main(string[] args)
         {
-            using (var context = new BlogDataContext())
+            using var context = new BlogDataContext();
+
+            var user = new User
             {
-                /* Create - NO TRACKING */
-                /* Defini os novos dados a serem persistidos */
-                // var tag = new Tag { Name = ".NET", Slug = "aspnet" };
-                // var tag2 = new Tag { Name = ".NET Core", Slug = "aspnet-core" };
-                /* Salva na Memória */
-                // context.Tags.Add(tag);
-                // context.Tags.Add(tag2);
-                /* Persiste no Banco de Dados */
-                // context.SaveChanges();
+                Name = "André Baltieri",
+                Slug = "andrebaltieri",
+                Email = "andre@balta.io",
+                Bio = "9x Microsoft MVP",
+                Image = "https://balta.io",
+                PasswordHash = "123098457"
+            };
 
-                /* ----------------------- */
+            var category = new Category
+            {
+                Name = "Backend",
+                Slug = "backend"
+            };
 
-                /* Update */
-                /* Busca o item no Banco de Dados */
-                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
-                /* Defini os dados a serem atualizados */
-                // tag.Name = ".NET";
-                // tag.Slug = "dotnet";
-                /* Atualiza na Memória */
-                // context.Update(tag);
-                /* Persiste no Banco de Dados */
-                // context.SaveChanges();
+            var post = new Post
+            {
+                Author = user,
+                Category = category,
+                Body = "<p>Hello world</p>",
+                Slug = "comecando-com-ef-core",
+                Summary = "Neste artigo vamos aprender EF core",
+                Title = "Começando com EF Core",
+                CreateDate = DateTime.Now,
+                LastUpdateDate = DateTime.Now,
+            };
 
-                /* ----------------------- */
-
-                /* Delete */
-                /* Busca o item no Banco de Dados */
-                // var tag = context.Tags.FirstOrDefault(x => x.Id == 1);
-                /* Exclui na Memória */
-                // context.Remove(tag);
-                /* Persiste no Banco de Dados */
-                // context.SaveChanges();
-
-                var tags = context
-                    .Tags /* Só até aqui não executa no banco */
-                    .AsNoTracking() /* Desabilita os Metadados (Usado só em Delete e Update) */
-                    .ToList(); /* Força a execução no banco (Sempre no final) */
-
-                foreach (var tag in tags)
-                    Console.WriteLine(tag.Name);
-            }
+            context.Posts.Add(post); // Não adicionei autor nem categoria
+            context.SaveChanges();
         }
     }
 }
