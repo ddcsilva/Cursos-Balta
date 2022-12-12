ec8ba5a573bf9276dcd6a29ac0e6c32bcc437c7f
Danilo Silva <danilo.silva@msn.com>
2022-12-12 13:55:32 -0300

    Mapeando Propriedades

--- Fundamentos_do_Entity_Framework/03_Mapeamento/Blog/Data/Mappings/CategoryMap.cs
+++ Fundamentos_do_Entity_Framework/03_Mapeamento/Blog/Data/Mappings/CategoryMap.cs
@@ -10,6 +10,27 @@ namespace Blog.Data.Mappings
         {
             // Tabela
             builder.ToTable("Category");
+
+            // Chave Primária
+            builder.HasKey(x => x.Id);
+
+            // Identity
+            builder.Property(x => x.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn(); // Primary Key Identity (1, 1)
+
+            // Propriedades
+            builder.Property(x => x.Name) // Define qual propriedade trabalhar
+                .IsRequired() // Constraint - Not Null
+                .HasColumnName("Name") // Nome da Coluna
+                .HasColumnType("NVARCHAR") // Tipo da Coluna
+                .HasMaxLength(80); // Máximo de Caracteres
+
+            builder.Property(x => x.Slug)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
         }
     }
 }
