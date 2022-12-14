7b4d78515029e7621de0aa5be8ad5efcd1242ab1
Danilo Silva <danilo.silva@msn.com>
2022-12-14 08:30:54 -0300

    Relacionamento muitos para muitos

--- Fundamentos_do_Entity_Framework/03_Mapeamento/Blog/Data/Mappings/PostMap.cs
+++ Fundamentos_do_Entity_Framework/03_Mapeamento/Blog/Data/Mappings/PostMap.cs
@@ -33,7 +33,7 @@ namespace Blog.Data.Mappings
                 .HasIndex(x => x.Slug, "IX_Post_Slug")
                 .IsUnique();
 
-            // Relacionamentos
+            // Relacionamento 1:N
             builder.HasOne(x => x.Author)
                 .WithMany(x => x.Posts)
                 .HasConstraintName("FK_Post_Author")
@@ -45,6 +45,23 @@ namespace Blog.Data.Mappings
                 .HasConstraintName("FK_Post_Category")
                 .OnDelete(DeleteBehavior.Cascade);
 
+            // Relacionamento N:N
+            builder
+                .HasMany(x => x.Tags)
+                .WithMany(x => x.Posts)
+                .UsingEntity<Dictionary<string, string>>(
+                    "PostTag",
+                    post => post.HasOne<Tag>()
+                        .WithMany()
+                        .HasForeignKey("PostId")
+                        .HasConstraintName("FK_PostTag_PostId")
+                        .OnDelete(DeleteBehavior.Cascade),
+                    tag => tag.HasOne<Post>()
+                        .WithMany()
+                        .HasForeignKey("TagId")
+                        .HasConstraintName("FK_PostTag_TagId")
+                        .OnDelete(DeleteBehavior.Cascade)
+                );
         }
     }
 }
