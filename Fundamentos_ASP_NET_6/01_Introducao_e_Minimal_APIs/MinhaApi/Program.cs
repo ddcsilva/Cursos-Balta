803a71ac7c3eb48d27e9110341da9d9dca0fead1
Danilo Silva <danilo.silva@msn.com>
2022-12-15 22:21:33 -0300

    Parâmetros

--- Fundamentos_ASP_NET_6/01_Introducao_e_Minimal_APIs/MinhaApi/Program.cs
+++ Fundamentos_ASP_NET_6/01_Introducao_e_Minimal_APIs/MinhaApi/Program.cs
@@ -3,8 +3,14 @@ var app = builder.Build(); // Vamos contruir a aplicação Web
 
 app.MapGet("/", () =>
 {
-    return "Hello World!";
+    return Results.Ok("Hello World!");
 });
+
+app.MapGet("/name/{nome}", (string nome) =>
+{
+    return Results.Ok($"Hello {nome}");
+});
+
 app.MapGet("/banana", () => "Banana é muito bom!");
 
 app.Run(); // Vamos colocar pra executar
