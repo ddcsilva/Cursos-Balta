db898b3a97c9d2ed04f3af29e857376db37bb278
Danilo Silva <danilo.silva@msn.com>
2022-12-15 22:16:29 -0300

    Funções anônimas

--- Fundamentos_ASP_NET_6/01_Introducao_e_Minimal_APIs/MinhaApi/Program.cs
+++ Fundamentos_ASP_NET_6/01_Introducao_e_Minimal_APIs/MinhaApi/Program.cs
@@ -1,7 +1,10 @@
 var builder = WebApplication.CreateBuilder(args); // Vamos criar uma aplicação Web
 var app = builder.Build(); // Vamos contruir a aplicação Web
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () =>
+{
+    return "Hello World!";
+});
 app.MapGet("/banana", () => "Banana é muito bom!");
 
 app.Run(); // Vamos colocar pra executar
