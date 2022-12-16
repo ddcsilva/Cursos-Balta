3b9b265baa20eacfc5dafa02f8aa869a16866fe6
Danilo Silva <danilo.silva@msn.com>
2022-12-15 22:12:38 -0300

    Como funciona um App ASP.NET

--- Fundamentos_ASP_NET_6/01_Introducao_e_Minimal_APIs/MinhaApi/Program.cs
+++ Fundamentos_ASP_NET_6/01_Introducao_e_Minimal_APIs/MinhaApi/Program.cs
@@ -1,6 +1,6 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+var builder = WebApplication.CreateBuilder(args); // Vamos criar uma aplicação Web
+var app = builder.Build(); // Vamos contruir a aplicação Web
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run();
+app.Run(); // Vamos colocar pra executar
