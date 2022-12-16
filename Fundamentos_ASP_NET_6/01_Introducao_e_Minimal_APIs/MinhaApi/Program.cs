8619a2140cc24a36af71cc248867e946d7c53c38
Danilo Silva <danilo.silva@msn.com>
2022-12-15 22:15:23 -0300

    Mapeando uma requisição

--- Fundamentos_ASP_NET_6/01_Introducao_e_Minimal_APIs/MinhaApi/Program.cs
+++ Fundamentos_ASP_NET_6/01_Introducao_e_Minimal_APIs/MinhaApi/Program.cs
@@ -2,5 +2,6 @@ var builder = WebApplication.CreateBuilder(args); // Vamos criar uma aplicação
 var app = builder.Build(); // Vamos contruir a aplicação Web
 
 app.MapGet("/", () => "Hello World!");
+app.MapGet("/banana", () => "Banana é muito bom!");
 
 app.Run(); // Vamos colocar pra executar
