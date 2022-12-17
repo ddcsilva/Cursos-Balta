b75aa3efeb6b1314d2b77eaf309d1525bcb7c945
Danilo Silva <danilo.silva@msn.com>
2022-12-16 21:57:02 -0300

    Criando um Registro

--- Fundamentos_ASP_NET_6/02_MVC/Todo/Controllers/HomeController.cs
+++ Fundamentos_ASP_NET_6/02_MVC/Todo/Controllers/HomeController.cs
@@ -12,5 +12,14 @@ namespace Todo.Controllers
         {
             return context.Todos.ToList();
         }
+
+        [HttpPost("/")]
+        public TodoModel Post([FromBody] TodoModel todo, [FromServices] AppDbContext context)
+        {
+            context.Todos.Add(todo);
+            context.SaveChanges();
+
+            return todo;
+        }
     }
 }
