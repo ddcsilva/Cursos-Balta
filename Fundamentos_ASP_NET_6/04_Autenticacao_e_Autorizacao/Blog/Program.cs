b3d08ea0d27b1f641a34f50c34e88fcfe4ad2ddc
Danilo Silva <danilo.silva@msn.com>
2022-12-18 20:55:19 -0300

    AddScoped, AddTransient, AddSingleton

--- Fundamentos_ASP_NET_6/04_Autenticacao_e_Autorizacao/Blog/Program.cs
+++ Fundamentos_ASP_NET_6/04_Autenticacao_e_Autorizacao/Blog/Program.cs
@@ -1,4 +1,5 @@
 using Blog.Data;
+using Blog.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -6,9 +7,10 @@ builder.Services.AddControllers()
     .ConfigureApiBehaviorOptions(options =>
     {
         options.SuppressModelStateInvalidFilter = true;
-    }); ;
+    });
 
 builder.Services.AddDbContext<BlogDataContext>();
+builder.Services.AddTransient<TokenService>();
 
 var app = builder.Build();
 
