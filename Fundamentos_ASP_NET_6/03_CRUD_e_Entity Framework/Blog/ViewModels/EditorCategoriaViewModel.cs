6aa474dc053bdfee44fe6c72919c294496e1e9ae
Danilo Silva <danilo.silva@msn.com>
2022-12-18 00:07:38 -0300

    Validações

--- Fundamentos_ASP_NET_6/03_CRUD_e_Entity Framework/Blog/ViewModels/EditorCategoriaViewModel.cs
+++ Fundamentos_ASP_NET_6/03_CRUD_e_Entity Framework/Blog/ViewModels/EditorCategoriaViewModel.cs
@@ -1,8 +1,13 @@
-﻿namespace Blog.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Blog.ViewModels
 {
     public class EditorCategoriaViewModel
     {
+        [Required(ErrorMessage = "O Nome é obrigatório!")]
+        [StringLength(40, MinimumLength = 3, ErrorMessage = "Este campo deve conter entre 3 e 40 caracteres")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "O Slug é obrigatório!")]
         public string Slug { get; set; }
     }
 }
