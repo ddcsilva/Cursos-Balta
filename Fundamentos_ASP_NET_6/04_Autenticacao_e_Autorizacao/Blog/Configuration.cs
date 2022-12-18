c07272c6cd95054c25f7dda374570922cea0e7d4
Danilo Silva <danilo.silva@msn.com>
2022-12-18 12:11:27 -0300

    Token e JWT

---
+++ Fundamentos_ASP_NET_6/04_Autenticacao_e_Autorizacao/Blog/Configuration.cs
@@ -0,0 +1,8 @@
+﻿namespace Blog
+{
+    public static class Configuration
+    {
+        // JWT - Json Web Token
+        public static string JwtKey { get; set; } = "ZGI1MWEzMzAtNGYyMy00MGEwLTllYjktYjc0MWMxODYzNDBi";
+    }
+}
