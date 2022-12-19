1d95c09bd31437000558d8697e45edf890f4b428
Danilo Silva <danilo.silva@msn.com>
2022-12-19 06:58:16 -0300

    Configurando Autenticação e Autorização

--- Fundamentos_ASP_NET_6/04_Autenticacao_e_Autorizacao/Blog/Controllers/AccountController.cs
+++ Fundamentos_ASP_NET_6/04_Autenticacao_e_Autorizacao/Blog/Controllers/AccountController.cs
@@ -1,4 +1,5 @@
 ﻿using Blog.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Blog.Controllers
@@ -9,8 +10,22 @@ namespace Blog.Controllers
         public IActionResult Login([FromServices]TokenService tokenService)
         {
             var token = tokenService.GenerateToken(null);
-
             return Ok(token);
         }
+
+        [Authorize(Roles = "user")]
+        [HttpGet("v1/user")]
+        public IActionResult GetUser() => Ok(User.Identity.Name);
+
+        [Authorize(Roles = "autor")]
+        [Authorize]
+        [HttpGet("v1/autor")]
+        public IActionResult GetAutor() => Ok(User.Identity.Name);
+
+        [Authorize(Roles = "admin")]
+        [Authorize]
+        [HttpGet("v1/admin")]
+        public IActionResult GetAdmin() => Ok(User.Identity.Name);
+
     }
 }
--- Fundamentos_ASP_NET_6/04_Autenticacao_e_Autorizacao/Blog/Services/TokenService.cs
+++ Fundamentos_ASP_NET_6/04_Autenticacao_e_Autorizacao/Blog/Services/TokenService.cs
@@ -17,6 +17,7 @@ namespace Blog.Services
                 Subject = new ClaimsIdentity(new Claim[]
                 {
                     new (ClaimTypes.Name, "danilosilva"),
+                    new (ClaimTypes.Role, "user"),
                     new (ClaimTypes.Role, "admin")
                 }),
                 Expires = DateTime.UtcNow.AddHours(8),
