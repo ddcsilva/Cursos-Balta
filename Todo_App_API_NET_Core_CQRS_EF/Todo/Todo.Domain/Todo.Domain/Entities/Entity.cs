e05bc86df69689838300a5ee14a80fd35d1cf91f
Danilo Silva <danilo.silva@msn.com>
2023-01-21 13:39:33 -0300

    Entidade Base

---
+++ Todo_App_API_NET_Core_CQRS_EF/Todo/Todo.Domain/Todo.Domain/Entities/Entity.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Todo.Domain.Entities
+{
+    public abstract class Entity: IEquatable<Entity>
+    {
+        protected Entity(Guid id)
+        {
+            Id = Guid.NewGuid();
+        }
+
+        public Guid Id { get; private set; }
+
+        public bool Equals(Entity other)
+        {
+            return Id == other.Id;
+        }
+    }
+}
