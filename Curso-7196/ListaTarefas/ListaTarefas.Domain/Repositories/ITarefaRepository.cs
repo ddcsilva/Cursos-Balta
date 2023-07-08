fce2a59db53ed60e1f20651bf7cf50b9f4653e0f
Danilo <danilo.silva@msn.com>
2023-07-08 20:43:53 -0300

    Começando o Handler

--- Curso-7196/ListaTarefas/ListaTarefas.Domain/Repositories/ITarefaRepository.cs
+++ Curso-7196/ListaTarefas/ListaTarefas.Domain/Repositories/ITarefaRepository.cs
@@ -2,6 +2,9 @@ using ListaTarefas.Domain.Entities;
 
 namespace ListaTarefas.Domain.Repositories;
 
+/// <summary>
+/// Interface para o repositório de tarefas.
+/// </summary>
 public interface ITarefaRepository
 {
     void Criar(Tarefa tarefa);
