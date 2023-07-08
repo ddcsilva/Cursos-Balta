cba31dbf2d4e0125ccdafe2a93f78dd838cfa67c
Danilo <danilo.silva@msn.com>
2023-07-08 18:40:14 -0300

    Atualizar Titulo da Tarefa Command

---
+++ Curso-7196/ListaTarefas/ListaTarefas.Domain/Commands/AlterarTituloCommand.cs
@@ -0,0 +1,31 @@
+using Flunt.Notifications;
+using Flunt.Validations;
+using ListaTarefas.Domain.Commands.Contracts;
+
+namespace ListaTarefas.Domain.Commands;
+
+public class AlterarTituloCommand : Notifiable, ICommand
+{
+    public AlterarTituloCommand() { }
+
+    public AlterarTituloCommand(Guid id, string titulo, string usuario)
+    {
+        Id = id;
+        Titulo = titulo;
+        Usuario = usuario;
+    }
+
+    public Guid Id { get; set; }
+    public string? Titulo { get; set; }
+    public string? Usuario { get; set; }
+
+    public void Validate()
+    {
+        AddNotifications(
+            new Contract()
+                .Requires()
+                .HasMinLen(Titulo, 3, "Titulo", "Por favor, descreva melhor esta tarefa!")
+                .HasMinLen(Usuario, 6, "Usuario", "Usuário inválido!")
+        );
+    }
+}
