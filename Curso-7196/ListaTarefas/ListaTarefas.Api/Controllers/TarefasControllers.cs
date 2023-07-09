998b45456d886caa9adb14b9e24f15c8496541b3
Danilo <danilo.silva@msn.com>
2023-07-09 12:49:21 -0300

    Criando uma Tarefa

--- Curso-7196/ListaTarefas/ListaTarefas.Api/Controllers/TarefasControllers.cs
+++ Curso-7196/ListaTarefas/ListaTarefas.Api/Controllers/TarefasControllers.cs
@@ -1,5 +1,7 @@
 using ListaTarefas.Domain.Commands;
+using ListaTarefas.Domain.Entities;
 using ListaTarefas.Domain.Handlers;
+using ListaTarefas.Domain.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ListaTarefas.Api.Controllers;
@@ -8,6 +10,13 @@ namespace ListaTarefas.Api.Controllers;
 [Route("api/[controller]")]
 public class TarefasController : ControllerBase
 {
+    [HttpGet]
+    [Route("")]
+    public IEnumerable<Tarefa> ObterTodasTarefas([FromServices] ITarefaRepository repository)
+    {
+        return repository.ObterTodasTarefas("danilo");
+    }
+
     [HttpPost]
     [Route("")]
     public GenericCommandResult CriarTarefa([FromBody] CriarTarefaCommand command, [FromServices] TarefaHandler handler)
