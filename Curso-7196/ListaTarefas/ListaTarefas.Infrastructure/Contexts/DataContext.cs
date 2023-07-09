9eebc4a4c92ae81551ae4388adfc85f0455321b3
Danilo <danilo.silva@msn.com>
2023-07-09 10:38:04 -0300

    Alterando os Mapeamentos

--- Curso-7196/ListaTarefas/ListaTarefas.Infrastructure/Contexts/DataContext.cs
+++ Curso-7196/ListaTarefas/ListaTarefas.Infrastructure/Contexts/DataContext.cs
@@ -11,4 +11,15 @@ public class DataContext : DbContext
     public DataContext(DbContextOptions<DataContext> options) : base(options) { }
 
     public DbSet<Tarefa> Tarefas { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Tarefa>().ToTable("Tarefas");
+        modelBuilder.Entity<Tarefa>().Property(x => x.Id);
+        modelBuilder.Entity<Tarefa>().Property(x => x.Titulo).HasMaxLength(120).HasColumnType("varchar(120)");
+        modelBuilder.Entity<Tarefa>().Property(x => x.DataConclusao);
+        modelBuilder.Entity<Tarefa>().Property(x => x.Usuario).HasMaxLength(120).HasColumnType("varchar(120)");
+        modelBuilder.Entity<Tarefa>().Property(x => x.Concluida).HasColumnType("bit");
+        modelBuilder.Entity<Tarefa>().HasIndex(x => x.Usuario);
+    }
 }
