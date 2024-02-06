e99a1d764bca2e3a3e62118d210c055c5a73eca0
danilo.silva <danilo.silva@msn.com>
2024-02-06 09:09:23 -0300

    Subtração

--- Fundamentos do CSharp/Modulo 06 - Calculadora/Calculadora/Program.cs
+++ Fundamentos do CSharp/Modulo 06 - Calculadora/Calculadora/Program.cs
@@ -1,6 +1,7 @@
 ﻿Console.Clear();
 
-Soma();
+// Soma();
+Subtracao();
 
 static void Soma()
 {
@@ -14,4 +15,20 @@ static void Soma()
 
     float resultado = primeiroValor + segundoValor;
     Console.WriteLine($"O resultado da soma é: {resultado}");
+    Console.ReadKey();
+}
+
+static void Subtracao()
+{
+    Console.WriteLine("Primeiro Valor: ");
+    float primeiroValor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo Valor: ");
+    float segundoValor = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("");
+
+    float resultado = primeiroValor - segundoValor;
+    Console.WriteLine($"O resultado da subtração é: {resultado}");
+    Console.ReadKey();
 }
