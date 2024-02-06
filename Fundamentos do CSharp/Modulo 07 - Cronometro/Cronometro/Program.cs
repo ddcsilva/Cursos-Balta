92629c6df81ce6701d4ff3f9789ac66d46e8b8d2
danilo.silva <danilo.silva@msn.com>
2024-02-06 11:08:57 -0300

    Estrutura do Cronometro + Thread e Sleep

---
+++ Fundamentos do CSharp/Modulo 07 - Cronometro/Cronometro/Program.cs
@@ -0,0 +1,20 @@
+﻿Iniciar();
+
+/// <summary>
+/// Inicia a contagem de tempo
+/// </summary>
+static void Iniciar()
+{
+    int tempo = 10;
+    int tempoAtual = 0;
+
+    while (tempoAtual != tempo)
+    {
+        Console.Clear(); // Limpa a tela
+
+        tempoAtual++;
+        Console.WriteLine("Tempo atual: " + tempoAtual);
+
+        Thread.Sleep(1000); // Aguarda 1 segundo
+    }
+}
