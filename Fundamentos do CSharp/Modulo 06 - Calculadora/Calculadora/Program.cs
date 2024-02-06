6a6792c68aa76db22e624264c904286d9a7a51f7
danilo.silva <danilo.silva@msn.com>
2024-02-06 09:04:50 -0300

    Soma

--- Fundamentos do CSharp/Modulo 06 - Calculadora/Calculadora/Program.cs
+++ Fundamentos do CSharp/Modulo 06 - Calculadora/Calculadora/Program.cs
@@ -1,5 +1,12 @@
 ﻿Console.Clear();
+
 Console.WriteLine("Primeiro Valor: ");
 float primeiroValor = float.Parse(Console.ReadLine());
 
-Console.WriteLine(primeiroValor);
+Console.WriteLine("Segundo Valor: ");
+float segundoValor = float.Parse(Console.ReadLine());
+
+Console.WriteLine("");
+
+float resultado = primeiroValor + segundoValor;
+Console.WriteLine($"O resultado da soma é: {resultado}");
