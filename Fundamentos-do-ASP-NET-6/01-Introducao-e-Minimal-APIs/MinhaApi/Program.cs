53e593e1c7649990bf9c6347bd9ee78bb53d18e7
danilo.silva <danilo.silva@msn.com>
2024-05-14 21:42:17 -0300

    Permitindo acesso ao HTTPS

--- Fundamentos-do-ASP-NET-6/01-Introducao-e-Minimal-APIs/MinhaApi/Program.cs
+++ Fundamentos-do-ASP-NET-6/01-Introducao-e-Minimal-APIs/MinhaApi/Program.cs
@@ -1,5 +1,18 @@
 var builder = WebApplication.CreateBuilder(args);
+
+// Configurações do Kestrel para ouvir em HTTP e HTTPS
+builder.WebHost.ConfigureKestrel(options =>
+{
+    options.ListenLocalhost(5273); // Porta HTTP
+    options.ListenLocalhost(7232, listenOptions =>
+    {
+        listenOptions.UseHttps(); // Porta HTTPS
+    });
+});
+
 var app = builder.Build();
+
+app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello World!");
 
