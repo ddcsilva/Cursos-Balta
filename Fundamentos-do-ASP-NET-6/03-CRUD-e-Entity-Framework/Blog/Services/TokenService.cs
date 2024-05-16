d5d2494ed6b444bf6d6ad74e5d2ceafe42434730
danilo.silva <danilo.silva@msn.com>
2024-05-16 00:52:03 -0300

    Token Service

---
+++ Fundamentos-do-ASP-NET-6/03-CRUD-e-Entity-Framework/Blog/Services/TokenService.cs
@@ -0,0 +1,19 @@
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Blog.Models;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Blog.Services;
+
+public class TokenService
+{
+    public string GerarToken(Usuario usuario)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Encoding.ASCII.GetBytes(Configuration.JwtKey);
+        var tokenDescriptor = new SecurityTokenDescriptor();
+        var token = tokenHandler.CreateToken(tokenDescriptor);
+
+        return tokenHandler.WriteToken(token);
+    }
+}
