525411175edd9cf842dd04aa5b7cbe9455c36116
danilo.silva <danilo.silva@msn.com>
2024-05-16 12:30:07 -0300

    Injeção de Dependência

--- Fundamentos-do-ASP-NET-6/03-CRUD-e-Entity-Framework/Blog/Configuration.cs
+++ Fundamentos-do-ASP-NET-6/03-CRUD-e-Entity-Framework/Blog/Configuration.cs
@@ -1,6 +1,6 @@
 namespace Blog;
 
-public class Configuration
+public static class Configuration
 {
     public static string JwtKey { get; set; } = "YzQ4MmNjNzYtMzRmOS00Zjg5LWFiZmYtMWI4Y2MyODgzNzZi";
 }
